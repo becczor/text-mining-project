f87646ff067f78f699ade0db353c66b82b77ff2f
Rebecca Lindblom <rebecca.lindblom@gmail.com>
2020-01-13 15:17:59 +0100

    rewritten to not read in the whole file, but rather take some lines at a time.

--- xml-to-text.py
+++ xml-to-text.py
@@ -2,54 +2,51 @@
 
 import re
 
-punctuation = [',', '-', "'", '*', '_', '.']
-
 def clean_sentence(text):
+	punctuation = [',', '-', "'", '*', '_', '.']
 	if text[-1] in punctuation:
 		text = text[:-1]
-
 	return text.lower()
 
+def xml_to_text(input_file, output_file):
+
+	start = '<sentence'
+	end = '</sentence>'
+	numbers = []
+	rf = open(input_file, 'r', encoding="utf-8")
+	wf = open(output_file, "a", encoding="utf-8")
+	print("both files opened")
+	started = False
+	# Read one line at a time
+	for line in rf:
+		# If there is an end in the line, the collected sentence should be processed.
+		if end in line:
+			started = False
+			# Find all raw words in sentence and add to a list
+			sentence_words = [re.findall(r'>.+<', content) for content in sentence]
+			sentence_words = [' '.join([word[1:-1] for word in words]) for words in sentence_words]
+
+			processed = ' '.join(sentence_words)
+			#processed = clean_sentence(' '.join(sentence_words))
+			try:
+				wf.write(processed + '\n')
+			except Exception as e:
+				print("Error: ", e)
+				print(processed)
+				raise e
+		# If we have started, the line should be appended to sentence. 
+		if started:
+			sentence.append(line)
+		# Reset sentence list and start over.
+		if start in line:
+			started = True
+			sentence = []
+
+	rf.close()
+	wf.close()
+	print("both files closed")
 
 
 
-def xml_to_text(filename):
 
-	f = open(filename, 'r', encoding="utf-8")
-	text = '\n'.join(f.readlines())
-
-
-	#print(text[4350760:4350770])
-	sentences_tags = re.findall(r'<sentence(?:(?!sentence>)[\s\S])*', text)
-
-	# Find all words, which can be found between
-	sentence_words = [re.findall(r'>.+<', content) for content in sentences_tags]
-	sentence_words = [' '.join([word[1:-1] for word in words]) for words in sentence_words]
-
-
-	#print(sentence_words[0:10])
-	w = open("sentences.txt", "a", encoding="utf-8")
-	print("file opened")
-
-	for sentence in sentence_words:
-		processed = clean_sentence(sentence)
-		try:
-			w.write(processed + '\n')
-		except Exception as e:
-			print("Error: ", e)
-			print(processed)
-			raise e
-		
-		
-
-	print("file written")
-	w.close()
-	print("file closed")
-	
-
-	
-
-
-
-xml_to_text("familjeliv-pappagrupp_2.xml")
-
+xml_to_text("flashback-livsstil.xml", "sentences.txt")
